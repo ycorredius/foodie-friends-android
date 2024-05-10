58555cd5b989e3b0c4201863178a3c81b522cca3
Shaun Yarbrough <ycorredius@gmail.com>
2024-05-10 17:51:11 -0400

    Updates page on load or after login to be DiscoverDestination.

--- app/src/main/java/com/example/foodiefriends/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/foodiefriends/ui/main/MainViewModel.kt
@@ -2,12 +2,10 @@ package com.example.foodiefriends.ui.main
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
-import com.example.foodiefriends.accessToken
-import com.example.foodiefriends.data.AuthRepository
 import com.example.foodiefriends.hasAccessToken
 import com.example.foodiefriends.printMsg
+import com.example.foodiefriends.ui.discover.DiscoverDestination
 import com.example.foodiefriends.ui.login.LoginDestination
-import com.example.foodiefriends.ui.dashboard.DashboardDestination
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -17,11 +15,11 @@ class MainViewModel @Inject constructor(
 	@ApplicationContext context: Context
 ) : ViewModel() {
 	private val hasAccessToken = hasAccessToken(context)
-	fun onAppStart(openAndPopUp: (String, String) -> Unit){
+	fun onAppStart(openAndPopUp: (String, String) -> Unit) {
 		printMsg("@@@@ HasAccessToken: $hasAccessToken")
-		if(hasAccessToken){
-			openAndPopUp(DashboardDestination.route, MainDestination.route)
-		} else{
+		if (hasAccessToken) {
+			openAndPopUp(DiscoverDestination.route, MainDestination.route)
+		} else {
 			openAndPopUp(LoginDestination.route, MainDestination.route)
 		}
 	}
